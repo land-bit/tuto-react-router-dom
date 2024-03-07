629c51cb678f7ee998a24e9bc1bf339f2c565c49
landrybit <landrybitege7@gmail.com>
2024-03-07 10:42:48 +0200

    add db, react router, front contact

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./routes/Root";
+import ErrorPage from "./ErrorPage";
+import Contact from "./routes/Contact";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [{ path: "/contacts/:contactId", element: <Contact /> }],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
